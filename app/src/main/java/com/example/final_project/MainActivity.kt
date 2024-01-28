688c4116f5986fb1a892ee9ce3589f1c61646f5c
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2024-01-29 01:27:08 +0400

    initial commit

---
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.final_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.final_project.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}

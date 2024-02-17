9ede969238569b210bf8eb75f414c4d9412ae3eb
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2024-02-18 01:36:47 +0400

    finished all sign up pages designs

---
+++ app/src/main/java/com/example/final_project/presentation/screen/signup/credentials/SignUpCredentialsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.final_project.presentation.screen.signup.credentials
+
+import com.example.final_project.databinding.FragmentSignUpCredentialsBinding
+import com.example.final_project.presentation.base.BaseFragment
+
+
+class SignUpCredentialsFragment : BaseFragment<FragmentSignUpCredentialsBinding>(FragmentSignUpCredentialsBinding::inflate) {
+    override fun setUp() {
+
+    }
+
+    override fun setUpListeners() {
+
+    }
+
+    override fun setUpObservers() {
+
+    }
+}
---
+++ app/src/main/java/com/example/final_project/presentation/screen/signup/success/SignUpSuccessFragment.kt
@@ -0,0 +1,25 @@
+package com.example.final_project.presentation.screen.signup.success
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.final_project.R
+import com.example.final_project.databinding.FragmentSignUpSuccessBinding
+import com.example.final_project.presentation.base.BaseFragment
+
+
+class SignUpSuccessFragment : BaseFragment<FragmentSignUpSuccessBinding>(FragmentSignUpSuccessBinding::inflate) {
+    override fun setUp() {
+
+    }
+
+    override fun setUpListeners() {
+
+    }
+
+    override fun setUpObservers() {
+
+    }
+}

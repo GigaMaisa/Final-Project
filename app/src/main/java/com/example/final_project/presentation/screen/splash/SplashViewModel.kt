2e42392ff2a3dc194821a7917f0960cd73e75941
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2024-02-17 20:00:21 +0400

    added sign in page

--- app/src/main/java/com/example/final_project/presentation/screen/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/final_project/presentation/screen/splash/SplashViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.final_project.presentation.screen.splash
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 class SplashViewModel: ViewModel() {
 

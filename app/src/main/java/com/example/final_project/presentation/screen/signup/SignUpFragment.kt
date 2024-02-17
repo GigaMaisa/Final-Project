323e7214b8c2f3f935de5e1b89631eb609291713
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2024-02-17 22:18:49 +0400

    added sign up flow's first page

---
+++ app/src/main/java/com/example/final_project/presentation/screen/signup/SignUpFragment.kt
@@ -0,0 +1,15 @@
+package com.example.final_project.presentation.screen.signup
+
+import com.example.final_project.databinding.FragmentSignUpBinding
+import com.example.final_project.presentation.base.BaseFragment
+
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate) {
+    override fun setUp() {
+    }
+
+    override fun setUpListeners() {
+    }
+
+    override fun setUpObservers() {
+    }
+}

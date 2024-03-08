7758225674957fed0faee7a727fb132404bcfac4
Giga Maisuradze <giga.maisuradze.1@btu.edu.ge>
2024-03-08 20:58:56 +0400

    fuck migrations

--- app/src/main/java/com/example/final_project/data/local/database/AppDataBase.kt
+++ app/src/main/java/com/example/final_project/data/local/database/AppDataBase.kt
@@ -1,5 +1,6 @@
 package com.example.final_project.data.local.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.final_project.data.local.dao.CardDao
@@ -7,7 +8,7 @@ import com.example.final_project.data.local.dao.DeliveryLocationDao
 import com.example.final_project.data.local.model.CardEntity
 import com.example.final_project.data.local.model.DeliveryLocationEntity
 
-@Database(entities = [DeliveryLocationEntity::class, CardEntity::class], version = 1, exportSchema = true)
+@Database(entities = [DeliveryLocationEntity::class, CardEntity::class], version = 2, exportSchema = true, autoMigrations = [AutoMigration(from = 1, to = 2)])
 abstract class AppDataBase: RoomDatabase() {
     abstract fun deliveryLocationDao(): DeliveryLocationDao
     abstract fun cardDao(): CardDao
